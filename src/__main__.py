7ba7159617bdffb119a45f2a5f8588af23fb8103
coloradoe <rxdelper@gmail.com>
2022-05-10 08:49:33 -0500

    Add Pulumi GKE cluster

---
+++ src/__main__.py
@@ -0,0 +1,114 @@
+import pulumi_gcp as gcp
+from pulumi import Config, export, get_project, get_stack, Output, ResourceOptions
+from pulumi_gcp.config import project, zone
+from pulumi_kubernetes import Provider
+from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
+from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs, Service, ServicePortArgs, ServiceSpecArgs
+from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
+
+# Read in some configurable settings for our cluster:
+config = Config(None)
+
+# max nodeCount is the max number of cluster nodes to autoscale out
+MAX_NODE_COUNT = config.get_int('max_node_count')
+# min nodeCount is the max number of cluster nodes to autoscale out
+MIN_NODE_COUNT = config.get_int('min_node_count')
+# nodeMachineType is the machine type to use for cluster nodes. Defaults to n1-standard-1 if unspecified.
+# See https://cloud.google.com/compute/docs/machine-types for more details on available machine types.
+NODE_MACHINE_TYPE = config.get('node_machine_type')
+# username is the admin username for the cluster.
+USERNAME = config.get('username') or 'admin'
+# password is the password for the admin user in the cluster.
+PASSWORD = config.get_secret('password')
+# master version of GKE engine
+MASTER_VERSION = config.get('master_version')
+# make cluster private
+PRIVATE_ENDPOINT = config.get('private_endpoint')
+# make cluster private
+PRIVATE_NODES = config.get('private_endpoint')
+# remove default node pool
+REMOVE_DEFAULT_NODEPOOL_ENABLED = config.get('remove_default_nodepool') or True
+# enable shielded nodes
+SHIELDED_NODES_ENABLED = config.get('enable_shielded_nodes') or True
+
+# Now, actually create the GKE cluster.
+k8s_cluster = gcp.container.Cluster('gke-cluster',
+    remove_default_node_pool=REMOVE_DEFAULT_NODEPOOL_ENABLED,
+    enable_shielded_nodes=SHIELDED_NODES_ENABLED,
+    initial_node_count=MIN_NODE_COUNT,
+    min_master_version=MASTER_VERSION,
+    private_cluster_config=gcp.container.ClusterPrivateClusterConfigArgs(
+        enable_private_endpoint=PRIVATE_ENDPOINT,
+        enable_private_nodes=PRIVATE_NODES,
+        master_ipv4_cidr_block="10.26.0.0/28"
+    ),
+    master_authorized_networks_config=gcp.container.ClusterMasterAuthorizedNetworksConfigArgs()
+)
+
+k8s_nodes = gcp.container.NodePool("primary-nodes",
+    cluster=k8s_cluster.id,
+    name="gke-demo-nodegroup",
+    autoscaling=gcp.container.NodePoolAutoscalingArgs(
+        max_node_count=MAX_NODE_COUNT,
+        min_node_count=MIN_NODE_COUNT
+    )
+)
+
+# Manufacture a GKE-style Kubeconfig. Note that this is slightly "different" because of the way GKE requires
+# gcloud to be in the picture for cluster authentication (rather than using the client cert/key directly).
+k8s_info = Output.all(k8s_cluster.name, k8s_cluster.endpoint, k8s_cluster.master_auth)
+k8s_config = k8s_info.apply(
+    lambda info: """apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: {0}
+    server: https://{1}
+  name: {2}
+contexts:
+- context:
+    cluster: {2}
+    user: {2}
+  name: {2}
+current-context: {2}
+kind: Config
+preferences: {{}}
+users:
+- name: {2}
+  user:
+    auth-provider:
+      config:
+        cmd-args: config config-helper --format=json
+        cmd-path: gcloud
+        expiry-key: '{{.credential.token_expiry}}'
+        token-key: '{{.credential.access_token}}'
+      name: gcp
+""".format(info[2]['cluster_ca_certificate'], info[1], '{0}_{1}_{2}'.format(project, zone, info[0])))
+
+# Make a Kubernetes provider instance that uses our cluster from above.
+k8s_provider = Provider('gke_k8s', kubeconfig=k8s_config)
+
+# Create a canary deployment to test that this cluster works.
+labels = { 'app': 'canary-{0}-{1}'.format(get_project(), get_stack()) }
+canary = Deployment('canary',
+    spec=DeploymentSpecArgs(
+        selector=LabelSelectorArgs(match_labels=labels),
+        replicas=1,
+        template=PodTemplateSpecArgs(
+            metadata=ObjectMetaArgs(labels=labels),
+            spec=PodSpecArgs(containers=[ContainerArgs(name='nginx', image='nginx')]),
+        ),
+    ), opts=ResourceOptions(provider=k8s_provider)
+)
+
+ingress = Service('ingress',
+    spec=ServiceSpecArgs(
+        type='LoadBalancer',
+        selector=labels,
+        ports=[ServicePortArgs(port=80)],
+    ), opts=ResourceOptions(provider=k8s_provider)
+)
+
+# Finally, export the kubeconfig so that the client can easily access the cluster.
+export('kubeconfig', k8s_config)
+# Export the k8s ingress IP to access the canary deployment
+export('ingress_ip', ingress.status.apply(lambda status: status.load_balancer.ingress[0].ip))
